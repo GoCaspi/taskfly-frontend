b8f17f8e5f59f4fc87459d91fbec5222bf8c933a
nil <n.spiller@gocaspi.de>
2022-11-08 17:18:01 +0100

    feature/ON4T-200_TaskCollection - debugged cors problems

--- src/app/myday/myday.component.ts
+++ src/app/myday/myday.component.ts
@@ -1,4 +1,20 @@
 import { Component } from '@angular/core';
+import axios from "axios";
+
+class TaskCollection {
+  members : string[] = ["1"];
+  collectionId : string = "1";
+  userId : string ="1";
+
+}
+interface User{
+  userId : string;
+
+}
+interface Task{
+  description : string;
+  userId : string;
+}
 
 @Component({
   selector: 'app-myday',
@@ -6,7 +22,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./myday.component.css']
 })
 export class MydayComponent   {
+user : User;
+actualUser : User = {userId:"1"}
+constructor() {
+  this.user = this.actualUser
+  let mDT = this.MyDayTasks()
+  console.log("User is :",this.user," and response from mock is :",mDT)
 
+}
 
-
+ MyDayTasks() : Task[]{
+let t :  Task[] = [{description:"",userId:""}]
+ let allTasks =   axios.get("http://localhost:8080/task/userId/1");
+   console.log("response from api :",allTasks)
+   return t
+}
 }
