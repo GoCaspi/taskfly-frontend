79ccbc5f9e52f3daccefddc333c45648c35faf8d
nil <n.spiller@gocaspi.de>
2022-11-25 15:02:45 +0100

    feature/ON4T-261_ResetPwd_ResetButton - create reset dialog that opens if openReset method is clicked via button

--- src/app/reset-dialog/reset-dialog.component.ts
+++ src/app/reset-dialog/reset-dialog.component.ts
@@ -15,9 +15,27 @@ export class ResetDialogComponent implements OnInit {
   }
 
   resetPwd(){
-    let resetBody = {lastName:this.lastNameInput,email:this.emailInput}
+    let resetBody = {lastName:"2",email:"abc"}
+   // let resetBody = {lastName:this.lastNameInput,email:this.emailInput}
     let resetBodyString = JSON.stringify(resetBody)
     this.http.post("http://localhost:8080/reset/", resetBodyString, {responseType: 'text'}).subscribe(r => console.log(r))
+  }
+
+  // TESTING PURPOSE, REMOVE THIS
+  async TestMETHOD() {
+    let taskString = JSON.stringify({
+      "userId":"1",
+      "listId":"MyDay",
+      "team":"blue",
+      "deadline":"11-11-2022",
+      "body":{
+        "topic":"new topic",
+        "priority":"new priority",
+        "description":"new description text"
+      }
+
+    })
+    this.http.post("http://localhost:8080/task/", taskString,{responseType: 'text'}).subscribe(r => console.log(r))
   }
 
 }
