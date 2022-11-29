b59816f53aefb773d3101d390f6c6fa5b92a9bac
nil <n.spiller@gocaspi.de>
2022-11-29 22:58:03 +0100

    feature/ON4T-261_ResetPwd_fixedBrokenUnitTests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,6 +5,7 @@ import {MAT_DIALOG_DATA, MAT_DIALOG_SCROLL_STRATEGY, MatDialog, MatDialogRef} fr
 import {ResetDialogComponent} from "./reset-dialog/reset-dialog.component";
 import {Dialog} from "@angular/cdk/dialog";
 import {By} from "@angular/platform-browser";
+import {EMPTY} from "rxjs";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -28,17 +29,28 @@ describe('AppComponent', () => {
   it(`should have as title 'TaskFly-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('TaskFly-frontend');
   });
 
   it('should have a button to reset the password. The inner text of the button element is : "Passwort vergessen?"', function () {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(fixture.debugElement.query(By.css('#openResetButton'))).toBeTruthy();
     expect(fixture.debugElement.query(By.css('#openResetButton')).nativeElement.innerHTML).toEqual("Passwort vergessen?");
   });
   it('should have a button-method openResetDialog, that opens the ResetDialog if clicked', function () {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const openDialogSpy = spyOn(app.dialog, 'open').and.returnValue({afterClosed: () => EMPTY} as any)
 
+    app.openReset();
+
+    expect(openDialogSpy).toHaveBeenCalled();
+    expect(openDialogSpy).toHaveBeenCalledWith(ResetDialogComponent);
   });
+
+
 
 });
