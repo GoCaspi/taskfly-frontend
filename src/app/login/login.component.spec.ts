8ff50fe784ad47464c771392823a7aad34889f0e
nil <n.spiller@gocaspi.de>
2022-12-01 12:58:13 +0100

    feature/ON4T-278_Login_supportNIL_codeSmells

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -4,29 +4,40 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import {HttpClient} from "@angular/common/http";
 import {AuthenticationService} from "../serives/authentication.service";
+import {createSpyFromClass, Spy} from "jasmine-auto-spies";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let service: AuthenticationService;
+  let authServiceSpy: Spy<AuthenticationService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
       providers: [
-        AuthenticationService,{
-          provide:AuthenticationService,
-          useValue:HttpClient
-        }
+        {provide:AuthenticationService,useValue: createSpyFromClass(AuthenticationService)}
         ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
+    service = TestBed.inject(AuthenticationService);
+    authServiceSpy = TestBed.inject<any>(AuthenticationService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+  });
+
+  it('should have a login method. Calling this method calls the service mehtod: login(). if the service doesnt return any error then the username and password gets saved to the local storage service.', () => {
+    //  const openDialogSpy = spyOn(component., 'open').and.returnValue({afterClosed: () => EMPTY} as any)
+    authServiceSpy.login.and.nextWith("")
+    component.loginUser()
+    expect(component).toBeTruthy();
+
   });
 });
