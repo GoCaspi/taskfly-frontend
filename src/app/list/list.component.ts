c9635814ca2a261cf2e919a11b4feaf0e0e8a580
nil <n.spiller@gocaspi.de>
2022-12-06 11:29:41 +0100

    -added listService - added html for listComponent

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {ListService} from "../serives/list.service";
+
+
+interface User{
+  userId : string;
+
+}
+interface TaskBody{
+  topic : string;
+  priority: string;
+  description: string;
+}
+
+interface Task{
+  body: TaskBody;
+  userId : string;
+  listId : string;
+  taskIdString : string;
+  team : string;
+  deadline : string;
+}
 
 @Component({
   selector: 'app-list',
@@ -7,10 +28,25 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  actualUser : User = {userId:"1"};
+  taskData : Task[]=[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private listService:ListService) { }
 
   ngOnInit(): void {
+    this.renderMyDayTasks()
   }
+  renderMyDayTasks(){
+    let tData : Task[] = [];
+    let myDayTasks : Task[] = [];
+    this.listService.getTasksOfList(this.actualUser.userId).subscribe(response =>{
+      tData = <Task[]>response;
+      tData.forEach(t =>{
+        if (t.listId == "MyDay"){myDayTasks.push(t);}
+      })
 
+      this.taskData = myDayTasks
+      console.log("tData on Object is :",this.taskData);
+    })
+  }
 }
---
+++ src/app/serives/list.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService {
+
+  constructor(private http: HttpClient) { }
+  getTasksOfList(userId: string){
+    return this.http.get("http://localhost:8080/task/userId/"+userId);
+  }
+}
