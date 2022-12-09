5eb2ac5b4bc0f57c02769cddafc9f075697fb25f
nil <n.spiller@gocaspi.de>
2022-12-09 16:58:13 +0100

    - styling for update-list-dialog

--- src/app/task-dialog/task-dialog.component.spec.ts
+++ src/app/task-dialog/task-dialog.component.spec.ts
@@ -1,17 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskDialogComponent } from './task-dialog.component';
+import {MAT_DIALOG_SCROLL_STRATEGY, MatDialog} from "@angular/material/dialog";
+import {TaskService} from "../serives/task.service";
+import {HttpClient, HttpHandler} from "@angular/common/http";
+import {ListService} from "../serives/list.service";
+import {createSpyFromClass, Spy} from "jasmine-auto-spies";
 
 describe('TaskDialogComponent', () => {
   let component: TaskDialogComponent;
   let fixture: ComponentFixture<TaskDialogComponent>;
-
+  let listServiceSpy: Spy<ListService>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskDialogComponent ]
+      declarations: [ TaskDialogComponent ],
+      providers:[MatDialog,{provide:MatDialog, useValue:MatDialog},{
+        provide : MAT_DIALOG_SCROLL_STRATEGY,
+        useValue : {}
+      },TaskService,{provide:TaskService, useValue: HttpClient}, HttpClient,ListService, HttpHandler]
     })
     .compileComponents();
-
+    listServiceSpy = TestBed.inject<any>(HttpClient);
     fixture = TestBed.createComponent(TaskDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
