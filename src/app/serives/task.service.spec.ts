0bd9cc7967f3133a40b86ba211dc0a8b21a78fc6
nil <n.spiller@gocaspi.de>
2022-12-13 12:14:38 +0100

    feature/ON4T-273_Frontend_Lists_Tests: - tests for appComponenent method: fetchAllListofUser

--- src/app/serives/task.service.spec.ts
+++ src/app/serives/task.service.spec.ts
@@ -3,8 +3,27 @@ import { TestBed } from '@angular/core/testing';
 import { TaskService } from './task.service';
 import {HttpClient, HttpHandler} from "@angular/common/http";
 import {createSpyFromClass, Spy} from "jasmine-auto-spies";
-import * as url from "url";
 
+interface TaskBody{
+  topic : string;
+  highPriority: string;
+  description: string;
+}
+
+interface Task{
+  body: TaskBody;
+  userId : string;
+  listId : string;
+  taskIdString : string;
+  team : string;
+  deadline : string;
+}
+interface TaskUpdate{
+  body: TaskBody;
+  listId : string;
+  team : string;
+  deadline : string;
+}
 describe('TaskService', () => {
   let service: TaskService;
   let httpSpy : Spy<HttpClient>
@@ -29,7 +48,7 @@ describe('TaskService', () => {
     expect(httpSpy.delete).toHaveBeenCalledWith('http://localhost:8080/task/' + mockID, Object({ responseType: 'text' }))
   });
 
-  it('should call the delete endpoint of the taskfly api and add the provided userId (param String) as query-parameter', () => {
+  it('should call the get endpoint of the taskfly api and add the provided userId (param String) as query-parameter', () => {
     let successMsg = "successfully deleted the task"
     let mockID = "123"
     let mockTask = {id:"",listId:"",}
@@ -55,7 +74,15 @@ describe('TaskService', () => {
         expect(httpSpy.get).toHaveBeenCalledWith('http://localhost:8080/task/taskId/' + tc.taskId )
       }
     })
+  });
 
-
+  it('should call the update endpoint of the taskfly api and add the provided userId (param String) as query-parameter', () => {
+    let successMsg = "successfully deleted the task"
+    let mockID = "123"
+    let mockTaskBody: TaskBody ={topic:"",highPriority:"",description:""}
+    let mockTask:Task ={body:mockTaskBody,userId:"",listId:"",taskIdString:"",team:"",deadline:""}
+    httpSpy.put.and.nextWith(successMsg)
+    service.updateTask(mockTask,mockID)
+    expect(httpSpy.put).toHaveBeenCalledWith('http://localhost:8080/task/123', mockTask, Object({ responseType: 'text' }))
   });
 });
